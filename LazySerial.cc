0b31fe233db7c4f82afe2fcda678f38d9533338e
James Clark <james@lazycat.com.au>
2016-08-29 19:20:26 +1000

    Assemble command maybe?

--- LazySerial.cc
+++ LazySerial.cc
@@ -19,25 +19,73 @@
 
 #include "LazySerial.h"
 
-//constructor
-LazySerial::LazySerial(
-		Stream &stream) :
-	d_stream(stream)
+namespace LazySerial
 {
-	clear_buffer();
-}
+	//constructor
+	LazySerial::LazySerial(
+			Stream &stream) :
+		d_stream(stream)
+	{
+		clear_buffer();
+		d_num_commands = 0;
+	}
+	
+	
+	void
+	LazySerial::loop()
+	{
+	}
+	
+	
+	void
+	LazySerial::register_callback(
+		const char* name,
+		CallbackFunction *callback)
+	{
+		d_commands[d_num_commands].name = name;
+		d_commands[d_num_commands].callback = callback;
+		d_num_commands++;
+	}
 
 
-void
-LazySerial::loop()
-{
-}
+	void
+	LazySerial::cmd_help()
+	{
+		d_stream.print("Available commands:");
+		for (int i = 0; i < d_num_commands; ++i) {
+			d_stream.print(" ");
+			d_stream.print(d_commands[i].name);
+		}
+		d_stream.print(".\n");
+	}
 
 
-void
-LazySerial::register_callback(
-	const char* name,
-	CallbackFunction *callback)
-{
-}
+	bool
+	LazySerial::assemble_command()
+	{
+		while (d_stream.available()) {
+			// Read new character
+			char ch = d_stream.read();
+			
+			// If it's the \n terminator, don't advance pos but instead write \0 and return success.
+			if (ch == '\n') {
+				d_buf[d_pos] = '\0';
+				return true;
+			}
+			
+			// For the mundane case, add the character to the buf and advance pos,
+			// keeping a \0 terminator just in case.
+			d_buf[d_pos] = ch;
+			d_pos++;
+			if (d_pos >= BUF_SIZE) {
+				// But if we're going to overflow, forget the whole damn thing.
+				clear_buffer();
+				return false;
+			}
+			d_buf[d_pos] = '\0';	// Just me being paranoid.
+		}
+		return false;
+	}
+	
+} //namespace
 
--- LazySerial.h
+++ LazySerial.h
@@ -20,78 +20,99 @@
 #ifndef LAZYSERIAL_H
 #define LAZYSERIAL_H
 
-#define BUF_SIZE 256	// Max size of any command string.
+#define BUF_SIZE 256	// Max size of any command string. Including the \0!
+#define NUM_CMDS 16  // Max number of commands we will ever have.
 
 #include <Arduino.h>
 
 
-/**
- * The function pointer signature used for callbacks.
- */
-typedef void (*CallbackFunction)(/*####Stream? Partial Buffer?*/);
-
-/**
- * The struct we store our callbacks in.
- */
-struct Callback
+namespace LazySerial
 {
-	const char *name;
-	CallbackFunction *callback;
-	Callback *next;	// considering this is Arduino, we may as well use a simple linked list...
-};
-
-
-class LazySerial
-{
-public:
 	/**
-	 * Constructor. Pass in the Stream to read and write from/to.
-	 * For example, the magic 'Serial' global variable should be usable as a Stream-class object.
+	 * The function pointer signature used for callbacks.
 	 */
-	explicit
-	LazySerial(
-		Stream &stream);
-	
-	/**
-	 * Call this from your own loop() for LazySerial to poll the Serial device for more data.
-	 * Shouldn't delay for too long unless one of your callbacks ends up triggering and taking time to process.
-	 */
-	void
-	loop();
-	
-	/**
-	 * Register a callback for use when a given command is seen.
-	 */
-	void
-	register_callback(
-		const char* name,
-		CallbackFunction *callback);
-		
-private:
-	/**
-	 * What stream we are reading from / writing to.
-	 */
-	Stream &d_stream;	
+	typedef void (*CallbackFunction)(const char *);
 
 	/**
-	 * Command Buffer, and our current position within it.
+	 * The struct we store our callbacks in.
 	 */
-	char d_buf[BUF_SIZE];
-	int  d_pos;
-	
-	/**
-	 * Where we store all the callbacks we have registered.
-	 * Lack of dynamic storage is killing me.
-	 */
-	
-	
-	void
-	clear_buffer()
+	struct Callback
 	{
-		d_pos = 0;
-		d_buf[d_pos] = '\0';
-	}
-};
+		const char *name;
+		CallbackFunction *callback;
+	};
 
+
+	class LazySerial
+	{  
+	public:
+		/**
+		 * Constructor. Pass in the Stream to read and write from/to.
+		 * For example, the magic 'Serial' global variable should be usable as a Stream-class object.
+		 */
+		explicit
+		LazySerial(
+			Stream &stream);
+		
+		/**
+		 * Call this from your own loop() for LazySerial to poll the Serial device for more data.
+		 * Shouldn't delay for too long unless one of your callbacks ends up triggering and taking time to process.
+		 */
+		void
+		loop();
+		
+		/**
+		 * Register a callback for use when a given command is seen.
+		 */
+		void
+		register_callback(
+			const char* name,
+			CallbackFunction *callback);
+			
+	private:
+		/**
+		 * What stream we are reading from / writing to.
+		 */
+		Stream &d_stream;	
+	
+		/**
+		 * Command Buffer, and our current position within it.
+		 */
+		char d_buf[BUF_SIZE];
+		int  d_pos;
+		
+		/**
+		 * Where we store all the callbacks we have registered.
+		 * Lack of dynamic storage is killing me.
+		 */
+		Callback d_commands[NUM_CMDS];
+		int      d_num_commands;
+		
+		void
+		clear_buffer()
+		{
+			d_pos = 0;
+			d_buf[d_pos] = '\0';
+		}
+		
+		/**
+		 * The magic HELP command is hard-coded to actually hit this method rather than anything in
+		 * the function table, because we want access to our list of commands.
+		 */
+		void
+		cmd_help();
+		
+		/**
+		 * Read bytes from the serial until we get a \n.
+		 * Returns true if we get one and have a completed command (with \0) in the buffer,
+		 * false if we have yet to get a full command (or hit BUF_SIZE - no incomplete command
+		 * will be processed)
+		 */
+		bool
+		assemble_command();
+		
+	};
+
+} //namespace
 
 #endif
