9b26754603f7c780d8eec2b28f4b5c613c5156df
James Neko <james.neko@gmail.com>
2025-05-15 18:27:18 +1000

    Parsing numbers

--- LazySerial/Context.h
+++ LazySerial/Context.h
@@ -17,9 +17,10 @@
  * <http://www.gnu.org/licenses/>.
  */
 #pragma once
-#include <string.h>  // C-style string functions strchr, strtok, etc.
+#include <stdlib.h>  // strtol, strtof
 
 #include "LazySerial/helpers.h"
+#include "LazySerial/parsing.h"
 
 
 namespace LazySerial
@@ -43,7 +44,8 @@ namespace LazySerial
       mode(m),
       stream(s),
       entered_command_name(nullptr),
-      args(nullptr) {  }
+      args(nullptr),
+      pos(nullptr)  {  }
 
     Context(
         CallingMode::CallingMode m,
@@ -53,11 +55,128 @@ namespace LazySerial
       mode(m),
       stream(s),
       entered_command_name(ecn),
-      args(a) {  }
+      args(a),
+      pos(a)  {  }
+
+
+    /**
+     * Consume whitespace from args. Update pos.
+     * You may land on \0, check for that.
+     */
+    void
+    parse_space() {
+      while (*pos && is_space(*pos)) {
+        pos++;
+      }
+    }
+    
+    /**
+     * Parse into some integer-like variable you supply by reference.
+     * Returns if parsing went ok.
+     * expect_hex: false if we don't know it's a hex number (we check for 0x), true if we know it must be hex (no 0x needed)
+     */
+    template<typename T>
+    bool
+    parse_int(T *var, bool expect_hex = false) {
+      // Consume leading whitespace.
+      parse_space();
+      LAZY_RETURN_FALSE_UNLESS(*pos);
       
+      // Test if hex.
+      if (is_hex_sigil(pos)) {
+        expect_hex = true;
+        pos += 2;
+      }
+      
+      char *end = pos;
+      long rval = strtol(pos, &end, expect_hex ? 16 : 10);
+      LAZY_RETURN_FALSE_UNLESS(end > pos);
+      
+      *var = rval;
+      pos = end;
+      return true;
+    }
+
+    /**
+     * Parse into some integer-like variable you supply by reference.
+     * Returns if parsing went ok.
+     * min / max: values that are the inclusive range allowed, returns error if outside.
+     * expect_hex: false if we don't know it's a hex number (we check for 0x), true if we know it must be hex (no 0x needed)
+     */
+    template<typename T>
+    bool
+    parse_int_minmax(T *var, T min, T max, bool expect_hex = false) {
+      // Consume leading whitespace.
+      parse_space();
+      LAZY_RETURN_FALSE_UNLESS(*pos);
+      
+      // Test if hex.
+      if (is_hex_sigil(pos)) {
+        expect_hex = true;
+        pos += 2;
+      }
+      
+      char *end = pos;
+      long rval = strtol(pos, &end, expect_hex ? 16 : 10);
+      LAZY_RETURN_FALSE_UNLESS(end > pos);
+      LAZY_RETURN_FALSE_UNLESS(rval >= min);
+      LAZY_RETURN_FALSE_UNLESS(rval <= max);
+      
+      *var = rval;
+      pos = end;
+      return true;
+    }
+
+    /**
+     * Parse into some float-like variable you supply by reference.
+     * Returns if parsing went ok.
+     */
+    template<typename T>
+    bool
+    parse_float(T *var) {
+      // Consume leading whitespace.
+      parse_space();
+      LAZY_RETURN_FALSE_UNLESS(*pos);
+      
+      char *end = pos;
+      double rval = strtod(pos, &end);
+      LAZY_RETURN_FALSE_UNLESS(end > pos);
+      
+      *var = rval;
+      pos = end;
+      return true;
+    }
+
+    
+    /**
+     * Parse into some float-like variable you supply by reference.
+     * min / max: values that are the inclusive range allowed, returns error if outside.
+     * Returns if parsing went ok.
+     */
+    template<typename T>
+    bool
+    parse_float_minmax(T *var, T min, T max) {
+      // Consume leading whitespace.
+      parse_space();
+      LAZY_RETURN_FALSE_UNLESS(*pos);
+      
+      char *end = pos;
+      double rval = strtod(pos, &end);
+      LAZY_RETURN_FALSE_UNLESS(end > pos);
+      LAZY_RETURN_FALSE_UNLESS(rval >= min);
+      LAZY_RETURN_FALSE_UNLESS(rval <= max);
+      
+      *var = rval;
+      pos = end;
+      return true;
+    }
+
+
     CallingMode::CallingMode mode;
     Stream &stream;
     const char *entered_command_name;
     char *args;  // pointer into d_buf
+    
+    char *pos;   // pointer into args
   }; // struct
 } // namespace
--- LazySerial/parsing.h
+++ LazySerial/parsing.h
@@ -18,23 +18,32 @@
  */
 #pragma once
 
-// Helper macros
+/**
+ * Helper for parsing. Hex or Dec is fine.
+ */
+inline
+bool
+is_int_digit(char ch) {
+  return (ch == '-' || ch == '+' || (ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'F') || (ch >= 'a' && ch <= 'f') || ch == 'x');
+}
 
-#define LAZY_STRINGIFY(X) #X
-#define LAZY_KEYVAL(X) " " #X "=" LAZY_STRINGIFY(X)
+/**
+ * Helper for parsing.
+ */
+inline
+bool
+is_space(char ch) {
+  return (ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n');
+}
 
-#define LAZY_RETURN_IF(X) if (X) { return; }
-#define LAZY_RETURN_UNLESS(X) if (!(X)) { return; }
 
-#define LAZY_RETURN_TRUE_IF(X) if (X) { return true; }
-#define LAZY_RETURN_TRUE_UNLESS(X) if (!(X)) { return true; }
-#define LAZY_RETURN_FALSE_IF(X) if (X) { return false; }
-#define LAZY_RETURN_FALSE_UNLESS(X) if (!(X)) { return false; }
+/**
+ * Check for '0x'
+ */
+inline
+bool
+is_hex_sigil(char *pos) {
+  return (pos[0] == '0' && pos[1] == 'x');
+}
 
-#ifndef MIN
-  #define MIN(a, b) (a) < (b) ? (a) : (b)
-#endif
-#ifndef MAX
-  #define MAX(a, b) (a) < (b) ? (b) : (a)
-#endif
 
