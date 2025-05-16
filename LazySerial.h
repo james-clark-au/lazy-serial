5ec9bb9c620ee8053dc51ce2ea863d8ae74c3c05
James Neko <james.neko@gmail.com>
2025-05-16 14:04:54 +1000

    Update changelog and licence

--- LazySerial.h
+++ LazySerial.h
@@ -1,20 +1,8 @@
 /*
  * This file is part of the LazySerial library.
- * Copyright (C) 2025 James Neko <arduino@neko.stream>
+ * Copyright (C) 2025 Lazy Cat Software <arduino@neko.stream>
  * 
- * LazySerial is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *          
- * LazySerial is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with LazySerial.  If not, see
- * <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: MIT
  */
 #pragma once
 #include <Arduino.h>
